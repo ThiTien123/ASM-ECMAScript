0734af0ec38b6c519d62a9495a9cb361e30af1ac
lathitien <tienltph19794@fpt.edu.vn>
2023-02-18 17:13:47 +0700

    update

--- src/page/admin/Projects.js
+++ src/page/admin/Projects.js
@@ -1,7 +1,32 @@
-
+import { projects } from "../../data";
+import { useEffect } from "../../utilities";
 const AdminProjectsPage = () => {
-  return `
-    <div><h1>AdminProjectsPage</h1></div>
+    useEffect(()=>{
+        const btns = document.querySelectorAll('.btn-remove');
+    });
+  return /*html*/`
+    <div class="container  mt-3">
+        <table>
+            <div class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>STT</th>
+                        <th>Tên</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                ${projects.map((project, index) =>`
+                    <tr>
+                        <td>${index +1}</td>
+                        <td>${project.name}</td>
+                        <td><button data-id="${project.id}" class="btn btn-danger btn-remove">Xóa</button></td>
+                    </tr>
+                    `).join("")}
+                </tbody>
+            </div>
+        </table>
+    </div>
   `
 }
 
